69847b92203db63a8641e26448c535bcba56cce2
Jun Chen <junc76@gmail.com>
2013-01-30 17:46:55 +0800

    Add the cartoon and rendertotex projects
    For the rendertotex project, the original code has an error. If the
    glClearColor is set to black when rendering to image, the cube will
    become invisible. You can understand it after reading the fragment
    shader.

---
+++ examples/cartoon/cartoon.cpp
@@ -0,0 +1,79 @@
+// Triangle.cpp
+// Our first OpenGL program that will just draw a triangle on the screen.
+
+#include <stdio.h>
+#include <GL/glew.h>
+#include <GL/glfw.h>
+
+#include "scenetoon.h"
+
+Scene * scene = NULL;
+
+///////////////////////////////////////////////////////////////////////////////
+// Window has changed size, or has just been created. In either case, we need
+// to use the window dimensions to set the viewport and the projection matrix.
+void ChangeSize(int w, int h)
+{
+    scene->resize(w, h);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// This function does any needed initialization on the rendering context. 
+// This is the first opportunity to do any OpenGL related tasks.
+void SetupRC()
+{
+    scene = new SceneToon();
+    scene->initScene();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Called to draw scene
+void RenderScene(void)
+{
+    scene->render();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Main entry point for GLUT based programs
+int main(int argc, char* argv[])
+{
+    int running = GL_TRUE;
+    // Initialize GLFW
+    if( !glfwInit() )
+    {
+        exit( EXIT_FAILURE );
+    }
+
+    // Open an OpenGL window
+    if( !glfwOpenWindow( 800,600, 0,0,0,0,0,0, GLFW_WINDOW ) )
+    {
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+
+    GLenum err = glewInit();
+    if ( GLEW_OK != err) {
+        fprintf(stderr , "GLEW Error: %s\n" , glewGetErrorString (err));
+        exit( EXIT_FAILURE );
+    }
+
+    SetupRC();
+
+    glfwSetWindowSizeCallback(ChangeSize);
+
+    // Main loop
+    while( running )
+    {
+        //// OpenGL rendering goes here...
+        RenderScene();
+        glfwSwapBuffers();
+
+        // Check if ESC key was pressed or window was closed
+        running = !glfwGetKey( GLFW_KEY_ESC ) &&
+            glfwGetWindowParam( GLFW_OPENED );
+    }
+    // Close window and terminate GLFW
+    glfwTerminate();
+    // Exit program
+    exit( EXIT_SUCCESS );
+}
